d743e4e1e876abe294f22338a907c852d36c3d6f
Owen <owen@easytarget.org>
2020-05-01 00:04:43 +0200

    Laser as a class with quadratic power mapping, other demo bits

--- Code/CatBit.py
+++ Code/CatBit.py
@@ -11,14 +11,6 @@ class Servo:
     """
     https://github.com/microbit-playground/microbit-servo-class
     For simplicity etc I just copy the class here rather than adding to IDE
-
-    A simple class for controlling hobby servos.
-    Args:
-        pin (pin0 .. pin3): The pin where servo is connected.
-        freq (int): The frequency of the signal, in hertz.
-        min_us (int): The minimum signal length supported by the servo.
-        max_us (int): The maximum signal length supported by the servo.
-        angle (int): The angle between minimum and maximum positions.
     """
 
     def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
@@ -44,16 +36,40 @@ class Servo:
         self.write_us(us)
 
     def disable(self):
+        self.pin.write_analog(0)  # pwm duty cycle to zero
         self.pin.write_digital(0)  # turn the pin off
 
+class Laser:
+    """
+    Set laser power via a exponential
+    """
+
+    def __init__(self, pin, max_pwm=1023, max_level=100):
+        self.pin = pin
+        self.max_pwm = max_pwm
+        self.max_level = max_level
+
+    def set_level(self, level=0):
+        # limit to range
+        if level > self.max_level:
+            level = self.max_level
+        if level < 0:
+            level = 0
+        pwm = pow(self.max_pwm + 1, level / self.max_level) - 1
+        self.pin.write_analog(pwm)
+
+    def off(self):
+        self.pin.write_analog(0)
+
 # Initialise pins etc
-svX = Servo(pin0,freq=50, min_us=680, max_us=2500, angle=180)
-svY = Servo(pin1,freq=50, min_us=680, max_us=2500, angle=180)
-pin2.set_analog_period(20)  # Laser PWM period
+# Values here are good for AliExpress G90 servos @5V
+svX = Servo(pin0, freq=50, min_us=700, max_us=2500, angle=180)
+svY = Servo(pin1, freq=50, min_us=700, max_us=2500, angle=180)
+led = Laser(pin2)
 
 # Limits
 min_x = 0
-max_x = 190
+max_x = 180
 min_y = 0
 max_y = 125
 center_x = min_x + (max_x - min_x)/2
@@ -62,58 +78,70 @@ center_y = min_y + (max_y - min_y)/2
 # Status
 centered = False
 
+# Functions
+def laserflash(speed=1):  # Silly demo function
+    for x in range(-100, 200, 3):
+        led.set_level(x)
+        sleep(speed)
+    for x in range(200, -100, -2):
+        led.set_level(x)
+        sleep(speed)
+
 while True:
-    if button_a.is_pressed():
+    if button_a.was_pressed():
         centered = False
         display.show("+")
         svX.write_angle(min_x)
         svY.write_angle(min_y)
-        pin2.write_analog(0)
-        sleep(2000)
-        for x in range(min_x, max_x, 2):
-            # from 0 to 180 in steps of 5
-            # write the angle of the step (x)
+        led.off()
+        sleep(1000)
+        for x in range(min_x, max_x, 1):
             svX.write_angle(x)
             svY.write_angle(x/1.4)
-            pin2.write_analog(x*5)
-            sleep(100)
-        sleep(1800)
-    if button_b.is_pressed():
+            led.set_level(100*(x-min_x)/(max_x-min_x))
+            sleep(30)
+        sleep(1000)
+        svX.write_angle(max_x)
+        svY.write_angle(min_y)
+        led.off()
+        for x in range(min_x, max_x, 1):
+            svX.write_angle(max_x-x)
+            svY.write_angle(x/1.4)
+            led.set_level(100*(x-min_x)/(max_x-min_x))
+            sleep(30)
+        sleep(1000)
+    if button_b.was_pressed():
         centered = False
-        pin2.write_analog(1023)
         display.show("O")
-        # show maximum and minimum rotation if button
-        # b pressed
-        svX.write_angle(min_x)
-        svY.write_angle(min_y)
-        sleep(2000)
-        svX.write_angle(min_x)
-        svY.write_angle(max_y)
-        sleep(2000)
         svX.write_angle(max_x)
         svY.write_angle(max_y)
-        sleep(2000)
+        sleep(500)
+        led.set_level(100)
+        sleep(1000)
         svX.write_angle(max_x)
         svY.write_angle(min_y)
-        sleep(2000)
+        sleep(1000)
         svX.write_angle(min_x)
         svY.write_angle(min_y)
-        sleep(2000)
-        pin2.write_analog(0)
+        sleep(1000)
+        svX.write_angle(min_x)
+        svY.write_angle(max_y)
+        sleep(1000)
+        svX.write_angle(max_x)
+        svY.write_angle(max_y)
+        sleep(1000)
+        led.off()
     else:
         display.show("-")
         # Turn off and center when no buttons pressed
         if not centered:
-            pin2.write_analog(0)
+            led.off()
             svX.write_angle(center_x)
             svY.write_angle(center_y)
             centered = True
-            sleep(1000)
+            sleep(500)
             svX.disable()
             svY.disable()
-        sleep(200)
-        if int(utime.ticks_ms()/1000) % 2 == 0:
-            pin2.write_analog(0)
-        else:
-            pin2.write_analog(1023)
+            sleep(500) # Need time for pwm to settle once turned off
+        laserflash()
 #fin
