2f28f449d0290fb0b9a4a6c0ebe2a5a7aafb83e2
Owen <owen@easytarget.org>
2020-05-02 16:08:37 +0200

    A working demo with cat entertainment and movement

--- Code/CatBit.py
+++ Code/CatBit.py
@@ -11,11 +11,6 @@ import random
 
 
 class Servo:
-    """
-    Original from https://github.com/microbit-playground/microbit-servo-class
-    Adapted, bugfixed and improved by Owen
-    For simplicity etc I just copy the class here rather than adding to IDE
-    """
 
     def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
         self.min_us = min_us
@@ -25,7 +20,7 @@ class Servo:
         self.angle = angle
         self.analog_period = 20
         self.pin = pin
-        analog_period = round((1/self.freq) * 1000)  # hertz to miliseconds
+        analog_period = round((1 / self.freq) * 1000)  # hertz to miliseconds
         self.pin.set_analog_period(analog_period)
 
     def write_us(self, us):
@@ -41,12 +36,20 @@ class Servo:
 
     def disable(self):
         # Dont turn off mid-pulse (avoids spurious movement)
-        start = utime.ticks_ms()   # short blocking loop
+        start = utime.ticks_ms()  # short blocking loop
         while utime.ticks_diff(utime.ticks_ms(), start) < 250:
             if not self.pin.read_digital():  # if pin is off
-                break                        # break out immediately
+                break  # break out immediately
         self.pin.write_digital(0)  # turn the pin off
 
+    def move(self, begin, end, duration):
+        # Move from start to end taking duration microseconds
+        # This is a blocking function for the duration of the move
+        start = utime.ticks_ms()  # short blocking loop
+        degrees_ms = float((end - begin) / duration)
+        while utime.ticks_diff(utime.ticks_ms(), start) <= duration:
+            self.write_angle(begin + (utime.ticks_diff(utime.ticks_ms(), start) * degrees_ms))
+            sleep(10)
 
 class LinearLed:
     """
@@ -74,96 +77,114 @@ class LinearLed:
     def off(self):
         self.pin.write_analog(0)
 
+
 # Initialise pins etc
 # Values here are good for AliExpress G90 servos @5V
 svX = Servo(pin0, freq=50, min_us=700, max_us=2500, angle=180)
 svY = Servo(pin1, freq=50, min_us=700, max_us=2500, angle=180)
-laser = LinearLed(pin2)
+la = LinearLed(pin2)
 
 # Limits
-min_x = 0
-max_x = 180
-min_y = 0
-max_y = 125
-center_x = min_x + (max_x - min_x)/2
-center_y = min_y + (max_y - min_y)/2
+minX = 0
+maxX = 180
+minY = 0
+maxY = 125
+midX = minX + (maxX - minX) / 2
+midY = minY + (maxY - minY) / 2
+# Light Sensor threshold (night->day) TODO
+# daylight_level = 36
 
 # Status
-centered = False
+home = False
 
 # Functions
-def Laserflash(speed=1):  # Flash the laser
+def Flash(speed=1):  # Flash the laser
     for x in range(-100, 200, 2):
-        laser.set_level(x)
+        la.set_level(x)
         sleep(speed)
     for x in range(200, -100, -2):
-        laser.set_level(x)
+        la.set_level(x)
         sleep(speed)
+
+
+def Home():  # 'Homes the turret and kills servos + laser
+    display.show(Image.DIAMOND_SMALL)
+    la.off()
+    svX.write_angle(midX)
+    svY.write_angle(midY)
+    sleep(500)  # wait for servos to settle
+    svX.disable()
+    svY.disable()
+
 
 def Demo1():  # Demo #1, draw crosses
     display.show(Image.NO)  # in this context, 'NO' is a big 'X'
-    svX.write_angle(min_x)
-    svY.write_angle(min_y)
-    laser.off()
+    svX.write_angle(minX)
+    svY.write_angle(minY)
+    la.off()
     sleep(1000)
-    for x in range(min_x, max_x, 1):
+    for x in range(minX, maxX, 1):
         svX.write_angle(x)
-        svY.write_angle(x/1.4)
-        laser.set_level(100*(x-min_x)/(max_x-min_x))
+        svY.write_angle(x / 1.4)
+        la.set_level(100 * (x - minX) / (maxX - minX))
         sleep(30)
     sleep(1000)
-    svX.write_angle(max_x)
-    svY.write_angle(min_y)
-    laser.off()
-    for x in range(min_x, max_x, 1):
-        svX.write_angle(max_x-x)
-        svY.write_angle(x/1.4)
-        laser.set_level(100*(x-min_x)/(max_x-min_x))
+    svX.write_angle(maxX)
+    svY.write_angle(minY)
+    la.off()
+    for x in range(minX, maxX, 1):
+        svX.write_angle(maxX - x)
+        svY.write_angle(x / 1.4)
+        la.set_level(100 * (x - minX) / (maxX - minX))
         sleep(30)
     sleep(1000)
 
 def Demo2():  # Demo #2, draw a bounds box
     display.show(Image.SQUARE)
-    svX.write_angle(max_x)
-    svY.write_angle(max_y)
+    svX.write_angle(midX)
+    svY.write_angle(maxY)
     sleep(500)
-    laser.set_level(100)
-    sleep(1000)
-    svX.write_angle(max_x)
-    svY.write_angle(min_y)
-    sleep(1000)
-    svX.write_angle(min_x)
-    svY.write_angle(min_y)
-    sleep(1000)
-    svX.write_angle(min_x)
-    svY.write_angle(max_y)
-    sleep(1000)
-    svX.write_angle(max_x)
-    svY.write_angle(max_y)
-    sleep(1000)
-    laser.off()
+    la.set_level(100)
+    svX.move(midX,minX,700)
+    svY.move(maxY,minY,1400)
+    svX.move(minX,maxX,1400)
+    svY.move(minY,maxY,1400)
+    svX.move(maxX,midX,700)
+    la.off()
 
-def Play(seconds=15, speed=10, led=100):
-    x = center_x * 60   # X position in seconds start at center
-    y = center_y * 60   # Y position in seconds start at center
+def Play(duration=45, speed=250, led=100):
+    # Angles are expressed as seconds (degrees*60)
+    x = midX * 60  # X position, start at center
+    y = midY * 60  # Y position, start at center
     # Turn the laser on
+    display.show(Image.TARGET)
     for i in range(0, led, 1):
-        laser.set_level(i)
+        la.set_level(i)
         sleep(5)
-    display.show(Image.PITCHFORK)
     start = utime.ticks_ms()
-    while utime.ticks_diff(utime.ticks_ms(), start) <= seconds * 1000:
-        x = x + random.randint(-1000, 1000)
-        y = y + random.randint(-800,800)
-        svX.write_angle(int(x/60))
-        svY.write_angle(int(y/60))
+    while utime.ticks_diff(utime.ticks_ms(), start) <= duration * 1000:
+        # Calculate a random offset to move
+        deltaX = random.randint(-1000, 1000)
+        deltaY = random.randint(-800, 800)
+        # Bounce off the play area edges
+        if (x + deltaX > maxX * 60) or (x + deltaX < minX * 60):
+            deltaX = -deltaX
+        if (y + deltaY > maxY * 60) or (y + deltaY < minY * 60):
+            deltaY = -deltaY
+        # Set new position and move
+        x = x + deltaX
+        y = y + deltaY
+        svX.write_angle(int(x / 60))
+        svY.write_angle(int(y / 60))
+        # Stop if either button pressed
         if button_a.was_pressed() or button_b.was_pressed():
             break
-        sleep(speed*30)
+        # Random pause based on the requested speed
+        sleep(random.randint(0, speed * 3))
 
     # Fade the laser down
     for i in range(led, 0, -1):
-        laser.set_level(i)
+        la.set_level(i)
         sleep(20)
 
 # Main Loop
@@ -171,20 +192,14 @@ while True:
     if button_a.was_pressed():
         # Demo1()
         Play()
-        centered = False
+        home = False
     if button_b.was_pressed():
         Demo2()
-        centered = False
+        home = False
     else:
-        display.show(Image.DIAMOND_SMALL)
-        # Turn off and center when no buttons pressed
-        if not centered:
-            laser.off()
-            svX.write_angle(center_x)
-            svY.write_angle(center_y)
-            centered = True
-            sleep(500)  # wait for servos to settle
-            svX.disable()
-            svY.disable()
-        Laserflash(speed=0)  # flash on/off as fast as possible
+        # Turn off and center as necesscary
+        if not home:
+            Home()
+            home = True
+        Flash(speed=0)  # flash on/off as fast as possible
 # fin
