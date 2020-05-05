20d0a20bb258a1a999579cfe6b19322e458f2eb8
Owen <owen@easytarget.org>
2020-05-05 02:30:37 +0200

    Beta Release * light sensing * timer on off * animations, countdowns and progress indicator * separate libraries

--- Code/CatBit.py
+++ Code/CatBit.py
@@ -6,76 +6,10 @@
 #   Pin3=Laser
 
 from microbit import *
+from ServoBit import Servo
+from LinearLed import LinearLed
 import utime
 import random
-
-
-class Servo:
-    def __init__(self, pin, freq=50, min_us=600, max_us=2400, angle=180):
-        self.min_us = min_us
-        self.max_us = max_us
-        self.us = 0
-        self.freq = freq
-        self.angle = angle
-        self.analog_period = 20
-        self.pin = pin
-        analog_period = round((1 / self.freq) * 1000)  # hertz to miliseconds
-        self.pin.set_analog_period(analog_period)
-
-    def write_us(self, us):
-        us = min(self.max_us, max(self.min_us, us))
-        duty = round(us * 1024 * self.freq // 1000000)
-        self.pin.write_analog(duty)
-
-    def write_angle(self, degrees=None):
-        degrees = degrees % 360
-        total_range = self.max_us - self.min_us
-        us = self.min_us + total_range * degrees // self.angle
-        self.write_us(us)
-
-    def disable(self):
-        # Allow pin to settle (avoids spurious movement)
-        self.pin.write_analog(0)  # pwm zero
-        start = utime.ticks_ms()  # short blocking loop
-        while utime.ticks_diff(utime.ticks_ms(), start) < 250:
-            if not self.pin.read_digital():  # if pin is off
-                break  # break out immediately
-        self.pin.write_digital(0)  # turn the pin off
-
-
-class LinearLed:
-    def __init__(self, pin, max_pwm=1023, max_level=100):
-        self.pin = pin
-        self.max_pwm = max_pwm
-        self.max_level = max_level
-        self.level = 0
-
-    def set_level(self, level=0):
-        # limit to range
-        if level > self.max_level:
-            level = self.max_level
-        if level < 0:
-            level = 0
-        # Do the calculation:
-        #  NB: the power function returns 1 when 'level' is 0
-        pwm = ((self.max_pwm + 1) ** (level / self.max_level)) - 1
-        # Write the value
-        self.pin.write_analog(pwm)
-        self.level = level
-
-    def off(self):
-        self.pin.write_analog(0)
-
-    def to(self, level, speed=1):
-        if level != self.level:
-            if level > self.level:
-                d = 1
-            else:
-                d = -1
-            for l in range(self.level, level, d):
-                self.set_level(l)
-                sleep(speed)
-
 
 # Initialise pins etc
 # Values here are good for AliExpress G90 servos @5V
@@ -88,11 +22,9 @@ minX = 30
 maxX = 150
 minY = 10
 maxY = 60
-midX = minX + (maxX - minX) / 2
-midY = minY + (maxY - minY) / 2
-# Light Sensor threshold (night->day) TODO
-# daylight_level = 36
-
+day = 40  # Light Sensor threshold
+timed = True
+gap = 5000000  # Gap between Plays, ms
 
 # Functions
 def Go(x, y):  # Simple instant XY move
@@ -100,28 +32,33 @@ def Go(x, y):  # Simple instant XY move
     svY.write_angle(y)
 
 
-def Move(x1, y1, x2, y2, duration=0):
+def Move(x1, y1, x2, y2, d=0):
     # Move from start to end taking duration microseconds
     # This is a blocking function for the duration of the move
     # Calling with zero duration is OK; an instant move
-    if duration > 0:
-        start = utime.ticks_ms()
-        x_per_ms = float((x2 - x1) / duration)
-        y_per_ms = float((y2 - y1) / duration)
+    if d > 0:
+        s = utime.ticks_ms()
+        xs = float((x2 - x1) / d)
+        ys = float((y2 - y1) / d)
         # This is the primary movement loop
-        while utime.ticks_diff(utime.ticks_ms(), start) <= duration:
-            tx = x1 + (utime.ticks_diff(utime.ticks_ms(), start) * x_per_ms)
-            ty = y1 + (utime.ticks_diff(utime.ticks_ms(), start) * y_per_ms)
-            Go(tx, ty)
-            sleep(10)  # We will recalculate position every 10ms
-    Go(x2, y2)  # ensures we end up on final position
+        while utime.ticks_diff(utime.ticks_ms(), s) <= d:
+            x = x1 + (utime.ticks_diff(utime.ticks_ms(), s) * xs)
+            y = y1 + (utime.ticks_diff(utime.ticks_ms(), s) * ys)
+            Go(x, y)
+            sleep(10)
+    Go(x2, y2)
 
 
-def Flash(level=100, speed=1):  # Flash the laser
-    la.to(level, speed)
-    sleep(speed * 2 * level)
-    la.to(0, speed)
-    sleep(speed * 2 * level)
+def Progress(s, d):
+    p = int(utime.ticks_diff(utime.ticks_ms(), s) / d * 25) % 25
+    for c in range(5):
+        for r in range(5):
+            if c * 5 + r < p:
+                display.set_pixel(c, 4 - r, pb)
+            elif c * 5 + r == p:
+                display.set_pixel(c, 4 - r, int(fc / 3) % (pb + 1))
+            else:
+                display.set_pixel(c, 4 - r, 0)
 
 
 def Home():  # Kills laser and homes the turret
@@ -129,32 +66,42 @@ def Home():  # Kills laser and homes the turret
     display.clear()
     svX.write_angle(midX)
     svY.write_angle(midY)
-    sleep(500)  # wait for servos to settle
+    sleep(500)  # wait for move
+    svY.disable()
+    svX.disable()
 
 
 def Setup():  # Draw a bounds box
-    display.show(Image.SQUARE)
-    svX.write_angle(midX)
-    svY.write_angle(minY)
-    sleep(500)
-    la.to(100)
-    Move(midX, minY, minX, minY, 700)
-    Move(minX, minY, minX, maxY, 1400)
-    Move(minX, maxY, maxX, maxY, 1400)
-    Move(maxX, maxY, maxX, minY, 1400)
-    Move(maxX, minY, midX, minY, 700)
-    la.to(0)
+    for i in range(3):
+        display.show(Image.ALL_CLOCKS, delay=33)
+    display.show(Image.CLOCK12)
+    sleep(330)
+    global timed
+    if button_b.was_pressed():
+        display.show(Image.SQUARE)
+        svX.write_angle(midX)
+        svY.write_angle(minY)
+        la.to(lb, 10)
+        Move(midX, minY, minX, minY, 700)
+        Move(minX, minY, minX, maxY, 1400)
+        Move(minX, maxY, maxX, maxY, 1400)
+        Move(maxX, maxY, maxX, minY, 1400)
+        Move(maxX, minY, midX, minY, 700)
+        la.to(0)
+    else:
+        timed = not timed
 
 
-def Play(duration=45, speed=250, led=100):
+def Play(duration=15, led=100):
     # Angles are expressed as seconds (degrees*60)
     x = midX * 60  # X position, start at center
     y = midY * 60  # Y position, start at center
+    duration *= 1000
     # Turn the laser on
-    display.show(Image.TARGET)
     la.to(led, 5)
     start = utime.ticks_ms()
-    while utime.ticks_diff(utime.ticks_ms(), start) <= duration * 1000:
+    while utime.ticks_diff(utime.ticks_ms(), start) <= duration:
+        Progress(start, duration)
         # Calculate a random duration and offset to move
         deltaT = random.randint(1, 8) * random.randint(100, 500)
         deltaX = random.randint(-1600, 1600)
@@ -170,38 +117,54 @@ def Play(duration=45, speed=250, led=100):
         y += deltaY
         # Stop if either button pressed
         if button_a.was_pressed() or button_b.was_pressed():
+            display.show(Image.NO)
             break
         # Random pause based on the requested speed
-        sleep(random.randint(0, speed * 3))
+        sleep(random.randint(0, 600))
     # Fade the laser down
     la.to(0, 30)
 
 
-# Init
-last_play = utime.ticks_ms()
-Home()
-display.show(Image.ALL_CLOCKS, loop=False, delay=66)
+# Init, splash animation
+last = 0
+display.show(Image.ALL_ARROWS, delay=66)
+display.show(Image.ARROW_N)
+sleep(330)
 servo = True
+midX = minX + (maxX - minX) / 2
+midY = minY + (maxY - minY) / 2
+pb = 3
+lb = 10
+fc = 0
 
 # Main Loop
 while True:
+    if servo:  # Servo has moved
+        Home()
+        servo = False
     if button_a.was_pressed():
-        Play()
-        last_play = utime.ticks_ms()
+        Play(60, lb)
+        last = utime.ticks_ms()
         servo = True
     elif button_b.was_pressed():
         Setup()
         servo = True
-    if servo:  # Servo has moved, so re-home and stop
-        Home()
-        svX.disable()
-        svY.disable()
-        servo = False
-    display.set_pixel(2, 2, 5)
-    if utime.ticks_diff(utime.ticks_ms(), last_play) > 5500000:
-        Flash(level=60, speed=3)  # flash laser on/off
-    if utime.ticks_diff(utime.ticks_ms(), last_play) > 6000000:
-        Play(duration=200, speed=250)
-        last_play = utime.ticks_ms()
-        servo = True
+    if timed:
+        Progress(last, gap)
+        if utime.ticks_diff(utime.ticks_ms(), last) > gap:
+            Play(200, lb)
+            last = utime.ticks_ms()
+            servo = True
+    else:
+        display.set_pixel(2, 2, pb)
+    if fc == 0:
+        if display.read_light_level() > day:
+            pb = 9
+            lb = 100
+        else:
+            pb = 5
+            lb = 66
+    fc += 1
+    fc %= (pb + 1) * 30
+    sleep(50 + 10 * (9 - pb))  # spend most of our time asleep
 # fin
---
+++ Code/LinearLed.py
@@ -0,0 +1,33 @@
+import utime
+
+
+class LinearLed:
+    def __init__(self, pin, max_pwm=1023, max_level=100):
+        self.pin = pin
+        self.max_pwm = max_pwm
+        self.max_level = max_level
+        self.level = 0
+
+    def set_level(self, level=0):
+        # limit to range
+        level = max(0, min(level, self.max_level))
+        # Do the calculation:
+        #  NB: the power function returns 1 when 'level' is 0
+        pwm = ((self.max_pwm + 1) ** (level / self.max_level)) - 1
+        # Write the value
+        self.pin.write_analog(pwm)
+        self.level = level
+
+    def off(self):
+        self.pin.write_analog(0)
+
+    def to(self, level, speed=1):
+        if level != self.level:
+            d = 1
+            if level < self.level:
+                d = -1
+            for l in range(self.level, level, d):
+                self.set_level(l)
+                start = utime.ticks_ms()
+                while utime.ticks_diff(utime.ticks_ms(), start) <= speed:
+                    pass
